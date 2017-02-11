e15235778e02f782eb6854469fb3792c814fd99c
llk23r <mail.acharyarahul.now@gmail.com>
2017-02-11 17:41:51 +0530

    maps commit 1

---
+++ app/src/main/java/com/dashconnect/dristikon/ProfileActivity.java
@@ -0,0 +1,13 @@
+package com.dashconnect.dristikon;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.material_design_profile_screen_xml_ui_design);
+    }
+}
